408cf1134339a84f53c5c61e446cb9a07a317842
mgechev <mgechev@gmail.com>
2013-08-27 22:38:33 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,66 @@
+var rfb = require('rfb2'),
+    socketio = require('socket.io').listen(8091, { log: false }),
+    Png = require('./node_modules/node-png/build/Release/png').Png,
+    connect = require('connect'),
+    clients = [];
+
+connect.createServer(connect.static('./static')).listen(8090);
+
+socketio.sockets.on('connection', function (socket) {
+    socket.on('init', function (data) {
+        var r = rfb.createConnection({
+            host: data.host,
+            port: data.port,
+            password: data.password
+        });
+        r.on('connect', function () {
+            socket.emit('init', {
+                width: r.width,
+                height: r.height
+            });
+            clients.push({
+                socket: socket,
+                rfb: r
+            });
+        });
+        r.on('rect', function (rect) {
+
+            var rgb = new Buffer(rect.width * rect.height * 3, 'binary'),
+                offset = 0;
+
+            for (var i = 0; i < rect.data.length; i += 4) {
+                rgb[offset++] = rect.data[i + 2];
+                rgb[offset++] = rect.data[i + 1];
+                rgb[offset++] = rect.data[i];
+            }
+
+            var image = new Png(rgb, r.width, r.height, 'rgb');
+            image = image.encodeSync();
+            socket.emit('frame', {
+                x: rect.x,
+                y: rect.y,
+                width: rect.width,
+                height: rect.height,
+                image: image.toString('base64')
+            });
+        });
+        socket.on('mouse', function (evnt) {
+            r.pointerEvent(evnt.x, evnt.y, evnt.button);
+        });
+        socket.on('keyboard', function (evnt) {
+            r.keyEvent(evnt.keyCode, evnt.isDown);
+        });
+
+    });
+
+    socket.on('disconnect', function () {
+        clients.forEach(function (pair) {
+            if (pair.socket === socket) {
+                pair.rfb.end();
+            }
+        });
+        clients = clients.filter(function (pair) {
+            return pair.socket === socket;
+        });
+    });
+});
---
+++ static/js/client.js
@@ -0,0 +1,116 @@
+(function () {
+
+    var Config = {
+        URL: 'http://localhost:8091'
+    };
+
+    function offset(elem) {
+        var rect = elem.getBoundingClientRect();
+        return {
+            top: rect.top + window.pageYOffset,
+            left: rect.left + window.pageXOffset
+        };
+    }
+
+    function Screen(canvas) {
+        this._canvas = canvas;
+        this._context = canvas.getContext('2d');
+    }
+
+    Screen.prototype.drawRect = function (rect) {
+        var img = new Image(),
+            that = this;
+        img.width = rect.width;
+        img.height = rect.height;
+        img.src = 'data:image/png;base64,' + rect.image;
+        img.onload = function () {
+            that._context.drawImage(this, rect.x, rect.y, rect.width, rect.height);
+        };
+    };
+
+    Screen.prototype.addMouseHandler = function (cb) {
+        var canvasOffset = offset(this._canvas);
+        this._canvas.addEventListener('mousedown', function (e) {
+            cb.call(null, e.pageX - canvasOffset.left, e.pageY - canvasOffset.top, 1);
+            e.preventDefault();
+        }, false);
+        this._canvas.addEventListener('mouseup', function (e) {
+            cb.call(null, e.pageX - canvasOffset.left, e.pageY - canvasOffset.top, 0);
+            e.preventDefault();
+        }, false);
+    };
+
+    Screen.prototype.addKeyboardHandlers = function (cb) {
+        document.addEventListener('keydown', function (e) {
+            cb.call(null, e.keyCode, 1);
+            e.preventDefault();
+        }, false);
+        document.addEventListener('keydown', function (e) {
+            cb.call(null, e.keyCode, 0);
+            e.preventDefault();
+        }, false);
+    };
+
+    Screen.prototype.getCanvas = function () {
+        return this._canvas;
+    };
+
+    function Client(screen) {
+        this._screen = screen;
+    }
+
+    Client.prototype._initEventListeners = function () {
+        var self = this;
+        this._screen.addMouseHandler(function (x, y, button) {
+            self._socket.emit('mouse', {
+                x: x,
+                y: y,
+                button: button
+            });
+        });
+        this._screen.addKeyboardHandlers(function (code, isDown) {
+            self._socket.emit('keyboard', {
+                keyCode: code,
+                isDown: isDown
+            });
+        });
+    };
+
+    Client.prototype.connect = function (config) {
+        var self = this;
+        this._socket = io.connect(Config.URL);
+        this._socket.emit('init', {
+            host: config.host,
+            port: config.port,
+            password: config.password
+        });
+        this._addHandlers();
+        this._initEventListeners();
+    };
+
+    Client.prototype._addHandlers = function () {
+        var that = this;
+        this._socket.on('init', function (config) {
+            var canvas = that._screen.getCanvas();
+            canvas.width = config.width;
+            canvas.height = config.height;
+        });
+        this._socket.on('frame', function (frame) {
+            that._screen.drawRect(frame);
+        });
+    };
+
+
+    document.getElementById('loginBtn').addEventListener('click', function () {
+        document.getElementById('authentication-table').style.display = 'none';
+        var screen = new Screen(document.getElementById('screen')),
+            client = new Client(screen);
+
+        client.connect({
+            host: document.getElementById('host').value,
+            port: parseInt(document.getElementById('port').value, 10),
+            password: document.getElementById('password').value,
+        });
+    }, false);
+
+}());
